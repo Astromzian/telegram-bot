eafb3da5fac578416b06866ec59540dabc66e929
Astromzian <vad-kozhevnikov@yandex.ru>
2021-04-22 17:00:27 +0300

    update reply command for bot 3

--- main.py
+++ main.py
@@ -35,8 +35,7 @@ def webhook():
     bot.set_webhook(url='https://mytelegramproject.herokuapp.com/' + TOKEN)  #
     return "!", 200
 
-
+bot.polling()
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-    bot.polling()
